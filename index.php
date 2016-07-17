f2bdb7b463d318f5e5883255bb1b16ee0e2d8fd6
webdevreaper <webdevreaper@users.noreply.github.com>
2016-07-17 16:43:49 +1000

    uploaded files to github

---
+++ index.php
@@ -0,0 +1,337 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Jason Grimston | Web Developer | Brisbane</title>
+	<!-- reset style -->
+	<link rel="stylesheet" type="text/css" href="/assets/style/reset.css">
+	<!-- fonts -->
+	<link href='https://fonts.googleapis.com/css?family=Fredoka+One|Lato:400,700' rel='stylesheet' type='text/css'>
+	<!-- my styles -->
+	<link rel="stylesheet" type="text/css" href="/assets/style/style.css">
+
+	<!-- jQuery - only used for Ajax -->
+	<script src="/assets/lib/externals/jquery/jquery-3.0.0.min.js"></script>
+</head>
+<body class="lato">
+<section class="main">
+	<section class="flex flex-1 full-height full-width vertical-center horizontal-center above-the-fold-wrapper">
+		<div class="above-the-fold flex vertical-center horizontal-center ">
+			<div class="logo-wrapper">
+				<!-- inline svg -->
+				<svg version="1.1" id="logo" xmlns="http://www.w3.org/2000/svg"
+				     xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+				     width="211px" height="295px" viewBox="0 0 211 295" enable-background="new 0 0 211 295"
+				     xml:space="preserve">
+					<g class="wdr">
+						<g class="wdr_cloak">
+							<path fill="#7143FF" d="M130.952,118.682c0,4.682,0.295,9.394-0.074,14.053
+									c-0.462,5.829-0.646,11.877-2.396,17.369c-5.11,16.027-20.576,26.89-37.375,26.933c-24.61,0.063-49.22,0.002-73.83,0.039
+									c-2.566,0.004-4.962-0.151-6.112-2.894c-1.154-2.752,0.406-4.621,2.252-6.375c1.393-1.324,2.672-2.771,4.097-4.059
+									c3.56-3.215,5.016-7.145,4.996-11.963c-0.103-25.634,0.054-51.27-0.087-76.902c-0.079-14.298,2.915-27.708,10.428-39.953
+									c2.172-3.54,0.661-5.847-1.825-8.371c-6.627-6.726-6.622-16.39,0.19-22.027c2.162-1.789,5.161-3.566,7.838-3.566
+									c2.676,0,30.091-0.948,44.905,0.72c26.26,2.955,46.747,27.187,47.003,53.662c0.104,10.879,1.175,33.21,1.175,33.21
+									L130.952,118.682z M47.089,133.273H27.76c0,3.679-0.196,7.122,0.046,10.535c0.349,4.922,4.616,8.583,9.614,8.583
+									c4.988,0,9.255-3.65,9.62-8.583C47.293,140.396,47.089,136.953,47.089,133.273"/>
+						</g>
+						<g class="wdr_cloak_fold_top">
+							<path fill="none" stroke="#FFFFFF" stroke-width="3" stroke-linecap="round"
+							      stroke-linejoin="round" stroke-miterlimit="10" d="
+				M32.04,15.156c0.844-2.051,2.216-3.939,4.118-5.513c2.162-1.789,5.19-2.773,7.838-2.798c1.057-0.01,2.113-0.012,3.171-0.009"/>
+						</g>
+						<g class="wdr_cloak_fold_bottom">
+							<path fill="none" stroke="#FFFFFF" stroke-width="3" stroke-linecap="round"
+							      stroke-miterlimit="10" d="
+				M121.852,147.384c-3.482,8.695-10.09,15.66-18.217,19.89"/>
+						</g>
+						<g class="wdr_skull">
+							<g class="wdr_face">
+								<path fill="#FFFFFF" stroke="#FFFFFF" stroke-linecap="round" stroke-linejoin="round"
+								      stroke-miterlimit="10" d="
+					M77.778,23.341c21.482,0,38.799,16.337,38.799,41.09c0,24.753-14.026,22.483-14.026,22.483v0.322c0,4.506-3.731,6.442-8.333,6.442
+					c-4.603,0-8.333-1.936-8.333-6.442c0,4.506-3.731,6.442-8.333,6.442c-4.603,0-8.334-1.936-8.334-6.442
+					c0,4.506-3.73,6.442-8.333,6.442c-4.602,0-8.333-1.936-8.333-6.442c0,0-13.573,0.03-13.573-22.805
+					C38.979,41.595,56.296,23.341,77.778,23.341"/>
+							</g>
+							<g class="wdr_eyes">
+								<g class="wdr_right_eye">
+									<path fill="none" stroke="#ED178E" stroke-width="3" stroke-linecap="round"
+									      stroke-linejoin="round" stroke-miterlimit="10" d="
+						M53.903,63.667c4.091-4.401,12.548-1.614,15.31,2.332"/>
+								</g>
+								<g class="wdr_left_eye">
+									<path fill="none" stroke="#ED178E" stroke-width="3" stroke-linecap="round"
+									      stroke-linejoin="round" stroke-miterlimit="10" d="
+						M101.653,62.818c-4.091-4.401-12.548-1.614-15.31,2.332"/>
+								</g>
+							</g>
+						</g>
+						<g class="wdr_left_arm">
+							<path fill="#7143FF" d="M139.45,89.581l-12.062-0.055l-1.336,23.89h13.026
+				c8.06-0.289,14.569-5.576,14.667-11.823C153.844,95.346,147.494,90.105,139.45,89.581"/>
+						</g>
+						<g class="wdr_left_hand">
+							<path fill="#FFFFFF" d="M141.133,110.181V92.842
+						c5.357,1.062,9.678,4.648,9.614,8.704C150.682,105.673,146.504,109.25,141.133,110.181"/>
+						</g>
+						<g class="wdr_right_hand">
+							<path fill="#FFFFFF" stroke="#FFFFFF" stroke-width="3" stroke-linecap="round"
+							      stroke-linejoin="round" stroke-miterlimit="10" d="
+	            M47.089,133.273H27.76c0,3.679-0.196,7.122,0.046,10.535c0.349,4.922,4.616,8.583,9.614,8.583
+	            c4.988,0,9.255-3.65,9.62-8.583C47.293,140.396,47.089,136.953,47.089,133.273"></path>
+						</g>
+						<g class="wdr_scythe">
+							<g class="wdr_scythe_handle">
+								<path fill="#ED178E" stroke="#ED178E" stroke-width="2" stroke-linecap="round"
+								      stroke-linejoin="round" stroke-miterlimit="10" d="
+					M155.146,41.291v-4.459c0-1.521-1.244-2.765-2.765-2.765h-2.11h-0.506h-2.111c-1.521,0-2.764,1.244-2.764,2.765v129.277
+					c0,1.521,1.243,2.767,2.764,2.767h2.111h0.506h2.11c1.521,0,2.765-1.244,2.765-2.767V56.779"/>
+							</g>
+							<g class="wdr_scythe_blade">
+								<path fill="#ED178E" stroke="#ED178E" stroke-width="2" stroke-linecap="round"
+								      stroke-linejoin="round" stroke-miterlimit="10" d="
+					M155.034,42.693c36.466,0,53.651,8.667,53.651,39.805c-6.937-21.071-22.399-25.719-53.539-25.719L155.034,42.693z"/>
+							</g>
+						</g>
+						<g class="wdr_text">
+							<g>
+								<g class="st8">
+									<path class="st9" d="M49.1,207.5c2.1,0.9,3.1,2,3.1,3.4c0,0.6-0.3,1.4-0.8,2.7c-0.5,1.2-1,2.3-1.3,3.2l-5.4,13.1
+					c-0.5,1.4-1.3,2.4-2.4,3c-0.5,0.2-1.1,0.3-1.9,0.3c-0.8,0-1.5-0.3-2.2-0.9c-0.7-0.6-1.2-1.2-1.4-1.7l-0.4-0.8
+					c-0.3-0.7-1.2-3.2-2.9-7.6c-0.3,0.8-0.8,2.1-1.4,3.8c-0.6,1.8-1,2.9-1.2,3.3c-0.2,0.4-0.3,0.7-0.3,0.9c0,0.1-0.2,0.4-0.4,0.8
+					c-0.2,0.4-0.5,0.7-0.8,1c-0.8,0.8-1.7,1.2-2.7,1.2c-1,0-1.9-0.3-2.5-0.9c-0.7-0.6-1.1-1.2-1.4-1.7l-7.4-18
+					c-0.3-1-0.5-1.6-0.5-1.9c0-1.3,0.9-2.3,2.8-3.1c1-0.4,1.9-0.6,2.5-0.6c0.7,0,1.2,0.1,1.6,0.4c0.4,0.3,0.7,0.6,0.9,0.8
+					c0.2,0.3,0.3,0.6,0.4,0.9c0.6,1.3,1.7,4.4,3.5,9.2l2.9-7.9c0.2-0.7,0.6-1.2,1.1-1.7c0.9-0.9,1.9-1.4,2.9-1.4s1.8,0.3,2.5,0.8
+					c0.7,0.5,1.2,1,1.4,1.6l0.4,0.8c1.6,4.6,2.6,7.2,2.9,7.8l3-8.1c0.8-2.1,2-3.1,3.5-3.1C47.8,207,48.5,207.2,49.1,207.5z"/>
+								</g>
+								<g class="st8">
+									<path class="st9" d="M79.9,217.1c0,2-0.5,3.5-1.5,4.5c-1,1-2.1,1.4-3.3,1.4H63.4c0,0.9,0.5,1.7,1.6,2.3c1.1,0.6,2.2,0.9,3.2,0.9
+					c1.9,0,3.4-0.2,4.5-0.6l0.5-0.2c0.8-0.4,1.4-0.5,2-0.5c1,0,1.9,0.7,2.7,2.2c0.4,0.9,0.6,1.6,0.6,2.3c0,2.9-3.5,4.4-10.4,4.4
+					c-2.4,0-4.6-0.4-6.4-1.2c-1.9-0.8-3.3-1.9-4.4-3.3c-2.1-2.6-3.1-5.6-3.1-8.9c0-4.1,1.3-7.4,4-9.9c2.7-2.5,6-3.7,10-3.7
+					c4.5,0,7.9,1.6,10,4.8C79.3,213.3,79.9,215.1,79.9,217.1z M69.5,218.6c1.2,0,1.9-0.5,1.9-1.6c0-0.8-0.3-1.4-0.9-1.8
+					c-0.6-0.5-1.4-0.7-2.4-0.7c-1.1,0-2.1,0.4-3.2,1.3c-1,0.9-1.6,1.8-1.6,2.8H69.5z"/>
+									<path class="st9" d="M97.2,207.4c2.9,0,5.6,1.3,8,3.9c2.4,2.6,3.6,5.6,3.6,9c0,3.4-1.2,6.4-3.6,9c-2.4,2.6-5,3.8-7.8,3.8
+					c-2.8,0-4.8-0.8-5.9-2.4c-0.3,1.6-1.7,2.4-4.2,2.4c-2.1,0-3.3-0.4-3.8-1.3c-0.2-0.5-0.4-0.9-0.4-1.3c0-0.4-0.1-0.9-0.1-1.7
+					v-29.2c0-0.8,0-1.3,0.1-1.7c0-0.4,0.2-0.8,0.4-1.2c0.5-0.9,1.7-1.3,3.8-1.3c2.1,0,3.3,0.4,3.7,1.3c0.3,0.5,0.4,0.9,0.5,1.3
+					c0,0.4,0.1,0.9,0.1,1.7v10.2C92.9,208.2,94.8,207.4,97.2,207.4z M92.9,223.2c0.8,0.9,1.8,1.3,3,1.3c1.2,0,2.2-0.4,3.1-1.3
+					c0.8-0.9,1.3-1.9,1.3-2.9c0-1.1-0.4-2.1-1.2-3c-0.8-0.9-1.8-1.3-3.1-1.3c-1.3,0-2.3,0.5-3.1,1.4c-0.8,0.9-1.2,1.9-1.2,3
+					C91.7,221.3,92.1,222.3,92.9,223.2z"/>
+								</g>
+								<g class="st8">
+									<path fill="#ED178E" stroke="#ED178E" d="M129.3,196.6c0.2-0.5,0.7-0.8,1.3-1c0.6-0.2,1.4-0.3,2.5-0.3s1.9,0.1,2.5,0.3c0.6,0.2,1.1,0.5,1.3,1
+					c0.2,0.5,0.4,0.9,0.4,1.3c0,0.4,0.1,0.9,0.1,1.7v29.2c0,0.8,0,1.3-0.1,1.7c0,0.4-0.2,0.8-0.5,1.2c-0.5,0.9-1.9,1.3-4.1,1.3
+					c-2.3,0-3.5-0.8-3.8-2.4c-1.4,1.6-3.3,2.4-5.7,2.4c-2.9,0-5.6-1.3-8-3.8c-2.4-2.6-3.6-5.6-3.6-9c0-3.4,1.2-6.4,3.6-9
+					c2.4-2.6,5.1-3.9,8-3.9c2.4,0,4.3,0.8,5.6,2.4v-10.3c0-0.8,0-1.3,0.1-1.7C129,197.5,129.1,197.1,129.3,196.6z M121.4,223.2
+					c0.8,0.9,1.9,1.3,3.1,1.3c1.2,0,2.2-0.4,3-1.3c0.8-0.9,1.2-1.9,1.2-2.9c0-1.1-0.4-2-1.2-3c-0.8-0.9-1.8-1.4-3.1-1.4
+					s-2.4,0.4-3.1,1.3c-0.8,0.9-1.2,1.9-1.2,3C120.1,221.3,120.5,222.3,121.4,223.2z"/>
+								</g>
+								<g class="st8">
+									<path fill="#ED178E" stroke="#ED178E" d="M166.2,217.1c0,2-0.5,3.5-1.5,4.5c-1,1-2.1,1.4-3.3,1.4h-11.6c0,0.9,0.5,1.7,1.6,2.3
+					c1.1,0.6,2.2,0.9,3.2,0.9c1.9,0,3.4-0.2,4.5-0.6l0.5-0.2c0.8-0.4,1.4-0.5,2-0.5c1,0,1.9,0.7,2.7,2.2c0.4,0.9,0.6,1.6,0.6,2.3
+					c0,2.9-3.5,4.4-10.4,4.4c-2.4,0-4.6-0.4-6.4-1.2c-1.9-0.8-3.3-1.9-4.4-3.3c-2.1-2.6-3.1-5.6-3.1-8.9c0-4.1,1.3-7.4,4-9.9
+					c2.7-2.5,6-3.7,10-3.7c4.5,0,7.9,1.6,10,4.8C165.6,213.3,166.2,215.1,166.2,217.1z M155.9,218.6c1.2,0,1.9-0.5,1.9-1.6
+					c0-0.8-0.3-1.4-0.9-1.8s-1.4-0.7-2.4-0.7c-1.1,0-2.1,0.4-3.2,1.3c-1,0.9-1.6,1.8-1.6,2.8H155.9z"/>
+								</g>
+								<g class="st8">
+									<path fill="#ED178E" stroke="#ED178E" d="M193.9,207.9c1.7,1,2.5,2.1,2.5,3.2c0,0.7-0.3,1.6-1,2.7l-10.1,17.4c-0.3,0.6-0.8,1.1-1.6,1.5
+					c-0.7,0.4-1.4,0.7-2.1,0.7c-0.7,0-1.4-0.2-2-0.5s-1.1-0.7-1.3-1.1l-0.4-0.5l-10.2-17.4c-0.7-1.1-1-2-1-2.6
+					c0-1.2,0.8-2.3,2.5-3.3c1.2-0.7,2.2-1.1,2.9-1.1s1.3,0.2,1.7,0.7c0.4,0.4,0.8,1.1,1.3,2l6.4,12.3l6.4-12.3
+					c0.4-0.6,0.6-1.1,0.8-1.3c0.2-0.3,0.5-0.6,0.9-0.8c0.4-0.3,0.9-0.4,1.6-0.4C191.9,206.9,192.8,207.2,193.9,207.9z"/>
+								</g>
+							</g>
+							<g>
+								<g class="st8">
+									<path class="st9" d="M36.7,252.7c0.7,0.2,1.3,0.6,1.8,1.2c0.5,0.5,0.7,1.4,0.7,2.6s-0.4,2.5-1.1,4c-0.7,1.4-1.8,2.1-3.1,2.1
+					c-0.7,0-1.3-0.1-1.9-0.5c-0.6-0.3-1.4-0.5-2.5-0.5c-1,0-2,0.4-2.9,1.1c-0.9,0.7-1.3,1.6-1.3,2.6v11.9c0,0.9,0,1.5-0.1,1.9
+					c-0.1,0.4-0.2,0.9-0.5,1.5c-0.6,1-2,1.5-4.4,1.5c-1.8,0-3.1-0.3-3.9-1c-0.6-0.5-0.9-1.3-1-2.4c0-0.4,0-0.9,0-1.6v-19.8
+					c0-0.9,0-1.5,0.1-2c0.1-0.4,0.2-0.9,0.5-1.4c0.5-1,2-1.5,4.4-1.5c2.3,0,3.7,0.4,4.2,1.3c0.4,0.6,0.6,1.2,0.6,1.9
+					c0.2-0.3,0.5-0.6,0.8-1c0.4-0.4,1.1-0.9,2.3-1.5c1.2-0.6,2.2-1,3.1-1c0.9,0,1.6,0,2.2,0.1C35.3,252.3,36,252.4,36.7,252.7z"/>
+								</g>
+								<g class="st8">
+									<path class="st9" d="M69.3,263.7c0,2.3-0.6,4-1.7,5.1c-1.1,1.1-2.4,1.7-3.8,1.7H50.4c0,1.1,0.6,1.9,1.9,2.7
+					c1.2,0.7,2.5,1.1,3.7,1.1c2.2,0,3.9-0.2,5.1-0.7l0.6-0.2c0.9-0.4,1.7-0.6,2.3-0.6c1.2,0,2.2,0.8,3.1,2.5c0.5,1,0.7,1.9,0.7,2.6
+					c0,3.4-4,5-12,5c-2.8,0-5.3-0.5-7.4-1.4c-2.2-1-3.9-2.2-5.1-3.8c-2.4-3.1-3.6-6.5-3.6-10.2c0-4.7,1.5-8.5,4.6-11.4
+					c3.1-2.8,6.9-4.3,11.5-4.3c5.2,0,9.1,1.8,11.5,5.5C68.7,259.2,69.3,261.3,69.3,263.7z M57.4,265.3c1.4,0,2.1-0.6,2.1-1.8
+					c0-0.9-0.3-1.6-1-2.1c-0.7-0.5-1.6-0.8-2.8-0.8c-1.2,0-2.4,0.5-3.6,1.5c-1.2,1-1.8,2.1-1.8,3.2H57.4z"/>
+									<path class="st9" d="M92.3,255.5c0.2-2.1,1.7-3.2,4.5-3.2c1.5,0,2.6,0.1,3.3,0.3c0.7,0.2,1.2,0.6,1.5,1.2
+					c0.3,0.6,0.5,1.1,0.5,1.5c0.1,0.4,0.1,1.1,0.1,2v19.8c0,0.9,0,1.5-0.1,1.9c-0.1,0.4-0.2,0.9-0.5,1.5c-0.5,1-1.8,1.5-3.9,1.5
+					c-2.1,0-3.4-0.3-4.1-0.8c-0.7-0.5-1.1-1.3-1.2-2.2c-1.4,2-3.7,3-7,3c-3.3,0-6.3-1.5-9.1-4.5c-2.8-3-4.2-6.4-4.2-10.4
+					c0-3.9,1.4-7.4,4.2-10.3c2.8-3,5.9-4.4,9.2-4.4c1.3,0,2.4,0.2,3.5,0.7c1.1,0.5,1.8,0.9,2.2,1.3
+					C91.7,254.7,92.1,255.1,92.3,255.5z M82,267.3c0,1.2,0.5,2.4,1.5,3.4c1,1,2.2,1.5,3.6,1.5c1.4,0,2.6-0.5,3.5-1.5
+					c0.9-1,1.4-2.1,1.4-3.4c0-1.2-0.4-2.4-1.3-3.4c-0.9-1.1-2.1-1.6-3.6-1.6c-1.5,0-2.7,0.5-3.6,1.6C82.5,264.9,82,266,82,267.3z"/>
+									<path class="st9" d="M106.7,290.2v-33c0-0.9,0-1.5,0.1-1.9c0.1-0.4,0.2-0.9,0.5-1.4c0.5-1,1.8-1.5,3.8-1.5c2,0,3.3,0.3,4,0.8
+					c0.7,0.5,1.1,1.3,1.2,2.2c1.4-2,3.7-2.9,6.8-2.9s6,1.5,8.7,4.4c2.7,2.9,4.1,6.3,4.1,10.1s-1.4,7.2-4.1,10.1
+					c-2.7,2.9-5.6,4.4-8.7,4.4c-3.1,0-5.3-0.9-6.7-2.6v11.4c0,0.9,0,1.5-0.1,1.9c-0.1,0.4-0.2,0.9-0.5,1.4c-0.3,0.5-0.8,0.9-1.4,1.2
+					c-0.7,0.2-1.6,0.3-2.8,0.3s-2.1-0.1-2.8-0.3c-0.7-0.2-1.2-0.6-1.5-1.1c-0.3-0.5-0.5-1-0.5-1.4
+					C106.7,291.7,106.7,291.1,106.7,290.2z M117.9,263.6c-0.9,1-1.3,2.1-1.3,3.3c0,1.2,0.4,2.3,1.3,3.3c0.9,1,2,1.5,3.4,1.5
+					s2.5-0.5,3.5-1.5c1-1,1.4-2.1,1.4-3.3s-0.5-2.4-1.4-3.4c-0.9-1-2.1-1.5-3.5-1.5S118.7,262.6,117.9,263.6z"/>
+									<path class="st9" d="M168.3,263.7c0,2.3-0.6,4-1.7,5.1c-1.2,1.1-2.4,1.7-3.8,1.7h-13.4c0,1.1,0.6,1.9,1.9,2.7
+					c1.2,0.7,2.5,1.1,3.7,1.1c2.2,0,3.9-0.2,5.1-0.7l0.6-0.2c0.9-0.4,1.7-0.6,2.3-0.6c1.2,0,2.2,0.8,3.1,2.5c0.5,1,0.7,1.9,0.7,2.6
+					c0,3.4-4,5-12,5c-2.8,0-5.3-0.5-7.4-1.4c-2.2-1-3.9-2.2-5.1-3.8c-2.4-3.1-3.6-6.5-3.6-10.2c0-4.7,1.5-8.5,4.6-11.4
+					c3.1-2.8,6.9-4.3,11.5-4.3c5.2,0,9.1,1.8,11.5,5.5C167.6,259.2,168.3,261.3,168.3,263.7z M156.4,265.3c1.4,0,2.1-0.6,2.1-1.8
+					c0-0.9-0.3-1.6-1-2.1c-0.7-0.5-1.6-0.8-2.8-0.8c-1.2,0-2.4,0.5-3.6,1.5c-1.2,1-1.8,2.1-1.8,3.2H156.4z"/>
+									<path class="st9" d="M192.2,252.7c0.7,0.2,1.3,0.6,1.8,1.2c0.5,0.5,0.7,1.4,0.7,2.6s-0.4,2.5-1.1,4c-0.7,1.4-1.8,2.1-3.1,2.1
+					c-0.7,0-1.3-0.1-1.9-0.5c-0.6-0.3-1.4-0.5-2.5-0.5c-1,0-2,0.4-2.9,1.1c-0.9,0.7-1.3,1.6-1.3,2.6v11.9c0,0.9,0,1.5-0.1,1.9
+					c-0.1,0.4-0.2,0.9-0.5,1.5c-0.6,1-2,1.5-4.4,1.5c-1.8,0-3.1-0.3-3.9-1c-0.6-0.5-0.9-1.3-1-2.4c0-0.4,0-0.9,0-1.6v-19.8
+					c0-0.9,0-1.5,0.1-2c0.1-0.4,0.2-0.9,0.5-1.4c0.5-1,2-1.5,4.4-1.5c2.3,0,3.7,0.4,4.2,1.3c0.4,0.6,0.6,1.2,0.6,1.9
+					c0.2-0.3,0.5-0.6,0.8-1c0.4-0.4,1.1-0.9,2.3-1.5s2.2-1,3.1-1c0.9,0,1.6,0,2.2,0.1C190.8,252.3,191.5,252.4,192.2,252.7z"/>
+								</g>
+							</g>
+						</g>
+					</g>
+				</svg>
+			</div>
+		</div>
+		<div class="arrow-down-svg">
+			<svg enable-background="new 0 0 32 32" height="32px" id="Layer_1" version="1.1" viewBox="0 0 32 32" width="32px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+				<path fill="rgba(2,2,2.6)" stroke="rgba(255,255,255,.8)" d="M14.77,23.795L5.185,14.21c-0.879-0.879-0.879-2.317,0-3.195l0.8-0.801c0.877-0.878,2.316-0.878,3.194,0  l7.315,7.315l7.316-7.315c0.878-0.878,2.317-0.878,3.194,0l0.8,0.801c0.879,0.878,0.879,2.316,0,3.195l-9.587,9.585  c-0.471,0.472-1.104,0.682-1.723,0.647C15.875,24.477,15.243,24.267,14.77,23.795z"/>
+			</svg>
+		</div>
+	</section>
+	<section class="flex flex-1 flex-direction-col horizontal-center page2-wrapper">
+		<section class="page2-section max-width-container">
+			<div class="about-section text-center">
+				<h1 class="fredoka-one">Need a Web Developer?</h1>
+				<p>Awesome! Because I'm looking for freelance work!</p>
+				<p>I'm the Senior Web Developer for <a href="http://www.siteloft.com" title="Siteloft" target="_blank">Siteloft</a>. Check out some of my recent work below and contact me for more information.</p>
+			</div>
+		</section>
+		<section class="page2-section">
+			<div class="project-section-title text-center">
+				<h1 class="fredoka-one no-margin">Projects</h1>
+			</div>
+			<div class="project-section flex flex-1">
+				<div class="flex flex-1 vertical-center horizontal-center  flex-wrap">
+					<section class="flex flex-1 vertical-center horizontal-center project-wrapper">
+						<div class="project-inner">
+							<a href="https://siteloft.com" title="Siteloft - corporate website" class="project-link" target="_blank">
+								<img src="/assets/images/projects/siteloft.png" alt="Siteloft" class="project-img">
+								<div class="hover-text-wrapper flex flex-1 vertical-center horizontal-center">
+									<div class="hover-text">
+										<h2 class="no-margin">Responsive WordPress site for Siteloft</h2>
+									</div>
+								</div>
+							</a>
+						</div>
+					</section>
+					<section class="flex flex-1 vertical-center horizontal-center project-wrapper">
+						<div class="project-inner">
+							<a href="http://rexsoftware.com.au" title="Rex Software - corporate website" class="project-link" target="_blank">
+								<img src="/assets/images/projects/rexsoftware.png" alt="Rex Software" class="project-img">
+								<div class="hover-text-wrapper flex flex-1 vertical-center horizontal-center">
+									<div class="hover-text">
+										<h2 class="no-margin">Responsive WordPress site for Rex Software</h2>
+									</div>
+								</div>
+							</a>
+						</div>
+					</section>
+					<section class="flex flex-1 vertical-center horizontal-center project-wrapper">
+						<div class="project-inner">
+							<a href="http://functionwell.com.au" title="Function well - corporate website" class="project-link" target="_blank">
+								<img src="/assets/images/projects/functionwell.png" alt="Function Well" class="project-img">
+								<div class="hover-text-wrapper flex flex-1 vertical-center horizontal-center">
+									<div class="hover-text">
+										<h2 class="no-margin">Responsive WordPress site for Function well</h2>
+									</div>
+								</div>
+							</a>
+						</div>
+					</section>
+				</div>
+			</div>
+		</section>
+		<section class="page2-section max-width-container">
+			<div class="text-center">
+				<h1 class="fredoka-one">Get in touch</h1>
+			</div>
+			<div class="form-section">
+				<div class="text-center">
+					<div id="form-messages"></div>
+				</div>
+				<form id="ajax-contact" method="post" action="mailer.php">
+					<section class="flex flex-1 flex-direction-col vertical-center horizontal-center fill-width">
+						<section class="flex horizontal-center fill-width">
+							<div class="fill-width input-wrapper field">
+								<input type="text" id="name" name="name" class="fill-width" placeholder="Name" required>
+							</div>
+							<div class="fill-width input-wrapper field">
+								<input type="email" id="email" name="email" class="fill-width" placeholder="Email" required>
+							</div>
+						</section>
+						<section class="flex horizontal-center fill-width">
+							<div class="fill-width input-wrapper field">
+								<textarea id="message" name="message" class="fill-width" placeholder="Message" required></textarea>
+							</div>
+						</section>
+						<section class="flex horizontal-center fill-width">
+							<div class="fill-width input-wrapper field">
+								<button type="submit" class="btn fredoka-one">Send</button>
+							</div>
+						</section>
+					</section>
+				</form>
+			</div>
+		</section>
+
+	</section>
+<script>
+	$(function fadeOnScroll(){
+		$(window).on("scroll", function(){
+			var distance = $(this).scrollTop();
+			if(distance <= 100) {
+				$('.arrow-down-svg svg').css("opacity", (1 - (distance/100)) );
+			}
+		});
+	});
+
+	/**
+	 * function that takes care of the ajax form submission
+	 *  - from http://blog.teamtreehouse.com/create-ajax-contact-form
+	 */
+	$(function() {
+		var form = $('#ajax-contact');
+		var formMessages = $('#form-messages');
+
+		$(form).submit(function (e) {
+			e.preventDefault();
+			var formData = $(form).serialize();
+			$.ajax({
+					type: 'POST',
+					url: $(form).attr('action'),
+					data: formData
+				})
+				.done(function (response) {
+					// Make sure that the formMessages div has the 'success' class.
+					$(formMessages).removeClass('error');
+					$(formMessages).addClass('success');
+
+					// Set the message text.
+					$(formMessages).text(response);
+
+					// Clear the form.
+					$('#name').val('');
+					$('#email').val('');
+					$('#msg').val('');
+
+					//hide the form
+					$('#ajax-contact').css("opacity",0);
+				})
+				.fail(function (data) {
+					// Make sure that the formMessages div has the 'error' class.
+					$(formMessages).removeClass('success');
+					$(formMessages).addClass('error');
+
+					// Set the message text.
+					if (data.responseText !== '') {
+						$(formMessages).text(data.responseText);
+					} else {
+						$(formMessages).text('Oops! An error occurred and your message could not be sent.');
+					}
+				});
+		});
+	});
+
+</script>
+</body>
+</html>
