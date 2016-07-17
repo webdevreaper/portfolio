deb80480fbfbdf9d8adb43a908d6c21a2be0ae97
Jason Grimston <jason.grimston@rexsoftware.com.au>
2016-07-17 18:41:18 +1000

    added ua code

--- index.php
+++ index.php
@@ -12,6 +12,19 @@
 
 	<!-- jQuery - only used for Ajax -->
 	<script src="/assets/lib/externals/jquery/jquery-3.0.0.min.js"></script>
+
+	<!-- Google Analytics -->
+	<script>
+		// Google Analytics
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-28010303-1', 'auto');
+		ga('send', 'pageview');
+		ga('clientTracker.send', 'pageview');
+	</script>
+
 </head>
 <body class="lato">
 <section class="main">
